5958ccfd1fdc4b6084de3d95b1196fa318f26017
Colin Mills <cgm71@georgetown.edu>
2019-03-25 19:34:17 -0400

    verifies file path

--- app/PDF_parser.py
+++ app/PDF_parser.py
@@ -1,18 +1,26 @@
 # credit for package https://pythonhosted.org/PyPDF2/
 import PyPDF2
+import os
 
 #pdfName = '/Users/colinmills/Desktop/Red2.pdf'
 #takes in file name as input 
-username = input("What is your username on your laptop")
-pdfName = input("Please enter the PDF file ion your desktop that you would like to be read: ") # a relative filepath
+username = input("What is your username on your laptop: ")
+pdfName = input("Please enter the PDF file name your desktop that you would like to be read: ") # a relative filepath
 
 pdfPath = "/Users/" + username + "/Desktop/" +pdfName
 
-read_pdf = PyPDF2.PdfFileReader(pdfPath)
+if os.path.isfile(pdfName):
 
-pages = input("How many pages, including the first, would you like there to be read.")
+    read_pdf = PyPDF2.PdfFileReader(pdfPath)
 
-for page in pages:
-    page = read_pdf.getPage(page)
-    page_content = page.extractText()
-    print(page_content)
+    pages = input("How many pages, including the first, would you like to be read: ")
+
+    page_num = int(pages)
+
+    for page in range(page_num):
+        page = read_pdf.getPage(page)
+        page_content = page.extractText()
+        print(page_content)
+
+else:
+    print("Sorry" + pdfPath + "doesn't appear to be a correct file path. \nPlease correct and try again.")
